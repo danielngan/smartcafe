18acc43b3dd863bfa21af3b9702d56a7d0623f6f
Vincent Ngan <tngan@my.centennialcollege.ca>
2024-01-01 17:31:08 -0500

    Add jest framework

---
+++ common/jest.config.js
@@ -0,0 +1,45 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: [
+    "."
+  ],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
+  ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "dist",
+    "tests-reports",
+    "frontend"
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  maxConcurrency: 1,
+  maxWorkers: 1,
+  collectCoverage: true,
+  coverageReporters: [
+    "json",
+    "html",
+    "text-summary",
+    "lcov",
+    "clover"
+  ],
+  coverageDirectory: "tests-reports/coverage",
+  reporters: [
+    "default",
+    ["jest-html-reporters", {
+      "publicPath": "./tests-reports",
+      "filename": "jest-report.html",
+      "expand": true,
+      enableMergeData: true,
+      dataMergeLevel: 2,
+      pageTitle: "SmartCafe Application Test Report",
+      includeConsoleLog: true,
+      }
+    ],
+  ]
+}
